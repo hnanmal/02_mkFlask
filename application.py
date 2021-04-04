c93b0396f45f2be6182c23bb46dadd8c47fd2cf7
Mankyu Jang <pjmk007@gmail.com>
2021-04-04 15:01:51 +0000

    asfd

--- application.py
+++ application.py
@@ -11,11 +11,12 @@ yList = []
 learning = False
 progress = ""
 
+
 # learner 모듈을 사용하여 학습
 def getWeightAndBias(epochs):
     global weightAndBias
     global learning
-    
+
     if not learning:
         learning = True
         weightAndBias = learner.learn(xList, yList, epochs)
@@ -31,15 +32,15 @@ def index():
 def learn():
     global xList
     global yList
-    
+
     xList = xList + list(map(float, request.json['xList'].strip().split(".")))
     yList = yList + list(map(float, request.json['yList'].strip().split(".")))
-    
+
     xList = xList[0: min(len(xList), len(yList))]
     yList = yList[0: min(len(xList), len(yList))]
-    
+
     getWeightAndBias(int(request.json['epochs']))
-    
+
     return
 
 # '예측하기' 요청시
@@ -47,26 +48,28 @@ def learn():
 def predict():
     return render(float(request.form['x']))
 
+
 # 데이터를 html 템플릿에 넣어 렌더링
 def render(x):
     global weightAndBias
     global learning
-    
+
     formular = f"[학습중{learner.progress}]" if learning else ""
     if weightAndBias is not None:
         formular += f"y = { weightAndBias[0] } * x + { weightAndBias[1] }"
     else:
         formular += "학습된 수식이 없습니다."
-    
+
     predicted = "학습된 수식이 없습니다."
     if (x is not None and weightAndBias is not None):
         predicted = (weightAndBias[0] * x + weightAndBias[1])
-        
+
     return render_template(
-    "index.html",
-    formular=formular,
-    predicted=predicted
-    )
+        "index.html",
+        formular=formular,
+        predicted=predicted
+        )
+
 
 if __name__ == "__main__":
     application.run(host='0.0.0.0', port=int(sys.argv[1]), debug=True)
--- learner.py
+++ learner.py
@@ -1,6 +1,7 @@
 # 모듈 로드
 import tensorflow as tf
 
+
 # 선형 모델 클래스
 class LinearModel:
     def __call__(self, x):
@@ -10,9 +11,11 @@ class LinearModel:
         self.Weight = tf.Variable(0.)
         self.Bias = tf.Variable(0.)
 
+
 # 오차(손실) 구하기 함수
 def loss(y, pred):
     return tf.reduce_mean(tf.square(y - pred))
+
 
 # 학습 함수
 def train(linear_model, x, y, lr):
@@ -25,23 +28,25 @@ def train(linear_model, x, y, lr):
     linear_model.Weight.assign_sub(lr * lr_weight)
     linear_model.Bias.assign_sub(lr * lr_bias)
 
+
 # 학습 경과 표시 문자열
 progress = ""
+
 
 # 학습 실행 함수
 def learn(xList, yList, epochs):
     linear_model = LinearModel()
     global progress
-    
+
     for epoch_count in range(epochs + 1):
         real_loss = loss(yList, linear_model(xList))
         train(linear_model, xList, yList, lr=0.003)
         progress = f": {epoch_count}/{epochs}"
         if epoch_count % 100 == 0:
             print(
-                f"{epoch_count}:: \:{linear_model.Weight.numpy()}" +
+                f"{epoch_count}:: q:{linear_model.Weight.numpy()}" +
                 f" b:{linear_model.Bias.numpy()} Loss: {real_loss.numpy}"
             )
-    
+
     progress = ""
     return (linear_model.Weight.numpy(), linear_model.Bias.numpy())
